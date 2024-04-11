b6c38b18f3252b74aabe18f43c71f2a897b11f81
Ing Duvan Rojas <duvanrojas99@gmail.com>
2024-04-11 09:56:38 -0500

    Fix to production 01

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
 // Iniciar el servidor
-app.listen(3000, () => {
-  console.log("Servidor iniciado en el puerto 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor iniciado en el puerto", process.env.PORT || "3000");
 });
