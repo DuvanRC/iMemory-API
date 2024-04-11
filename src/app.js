67bf58bc709e17d52d713fad628013d48a9879ed
Ing Duvan Rojas <duvanrojas99@gmail.com>
2024-04-11 11:50:59 -0500

    Fix to production 02

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import routes from "./routes/index.js";
+import path from "path";
 
 const app = express();
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 
 // Configurar moddlewares
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+console.log(path.resolve("./firebase.json"));
 
 // Configurar rutas
 app.use("/api", routes);
